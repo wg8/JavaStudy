086a3bcf7ef7650feb866d6fbbf512311e823909
Harry <null>
2019-04-22 23:17:33 +0800

    更新 package-info.java

--- src/main/java/com/wg8/gof23/package-info.java
+++ src/main/java/com/wg8/gof23/package-info.java
@@ -2,5 +2,20 @@
  * @Author Harry
  * @Date 2019/3/29
  * @Description 主要介绍 23 种设计模式
+ * <p>
+ * 总体来说设计模式分为三大类：
+ * <p>
+ * 创建型模式，共五种：
+ * 工厂方法模式（factorymethod）、抽象工厂模式（abstractfactory）
+ * 单例模式（singleton）、建造者模式（builder）、原型模式（prototype）
+ * <p>
+ * 结构型模式，共七种：
+ * 适配器模式（adapter）、装饰器模式（decorator）、代理模式（proxy）、
+ * 外观模式（facade）、桥接模式（bridge）、组合模式（composite）、享元模式（flyweight）
+ * <p>
+ * 行为型模式，共十一种：
+ * 策略模式（strategy）、模板方法模式（templateMethod）、观察者模式（observer）、迭代器模式（iterator）
+ * 责任链模式（chainOfResp）、命令模式（command）、备忘录模式（memento）、状态模式（state）
+ * 访问者模式（visitor）、中介者模式（mediator）、解释器模式（interpreter）
  */
 package com.wg8.gof23;
